5b6a00bfe70a8a73d3b6d72273dfc1c88a7c3239
Reilly Watson <reillywatson@gmail.com>
2017-12-12 21:04:04 -0500

    try to call out to MLS

--- houses.go
+++ houses.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/feeds"
+	"html/template"
 	"net/http"
-	"strings"
+	"net/url"
 	"time"
 )
 
@@ -18,12 +22,23 @@ type houseListing struct {
 	MLSNumber    string   `json:"MLS#"`
 	Tags         []string `json:"Undefined"`
 	Pictures     []picture
+	MLSListing   MLSListing `json:"-"`
 }
 
 type picture struct {
 	Url         string `json:"url"`
 	Description string `json:"description"`
 }
+
+var descriptionTemplate = template.Must(template.New("desc").Parse(
+	`
+	<p>
+	{{.remarks}}
+	</p>
+	<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?&q={{.latitude}},{{.longitude}}" allowfullscreen></iframe>
+	<p/>
+	<a href="{{.mls_link}}">MLS</a>
+	`))
 
 func getHouses(w http.ResponseWriter, _ *http.Request) {
 	resp, err := http.Get("https://www.mongohouse.com/api/newlistings?query=true&price_min=$300,000&price_max=$750,000&list_day_back=120&bedrooms=2&washrooms=1&ownershiptype=freehold&openhouse=undefined&south=43.653101797558975&west=-79.4750231366699&north=43.71008371751932&east=-79.28756891303709")
@@ -37,16 +52,84 @@ func getHouses(w http.ResponseWriter, _ *http.Request) {
 		Title: "House listings",
 		Link:  &feeds.Link{Href: "http://mongohouse.com"},
 	}
-	for _, listing := range listings {
+	for _, listing := range listings[:5] {
+		mls, err := getMLS(listing.MLSNumber)
+		if err != nil {
+			mls = &MLSListing{PublicRemarks: err.Error()}
+		}
+		mlsUrl := fmt.Sprintf("https://www.realtor.ca%s", mls.RelativeDetailsURL)
+		var buf bytes.Buffer
+		descriptionTemplate.Execute(&buf, map[string]interface{}{
+			"longitude": mls.Property.Address.Longitude,
+			"latitude":  mls.Property.Address.Latitude,
+			"remarks":   mls.PublicRemarks,
+			"mls_link":  mlsUrl,
+		})
 		listingDate, _ := time.Parse("01/02/2006", listing.ContractDate)
 		item := &feeds.Item{
 			Title:       fmt.Sprintf("%s - %s", listing.Address1, listing.ListPrice),
 			Link:        &feeds.Link{Href: fmt.Sprintf("https://mongohouse.com/newlistings/%s", listing.Id)},
 			Created:     listingDate,
-			Description: strings.Join(listing.Tags, ", ") + " \nMLS ID: " + listing.MLSNumber,
+			Description: buf.String(),
 		}
 		feed.Items = append(feed.Items, item)
 	}
 	rss, _ := feed.ToRss()
 	w.Write([]byte(rss))
+}
+
+type MLSListing struct {
+	Building struct {
+		BathroomTotal string `json:"BathroomTotal"`
+		Bedrooms      string `json:"Bedrooms"`
+		StoriesTotal  string `json:"StoriesTotal"`
+		Type          string `json:"Type"`
+	} `json:"Building"`
+	Property struct {
+		Address struct {
+			AddressText string `json:"AddressText"`
+			Longitude   string `json:"Longitude"`
+			Latitude    string `json:"Latitude"`
+		} `json:"Address"`
+		Photos []struct {
+			HighResPath string `json:"HighResPath"`
+			MedResPath  string `json:"MedResPath"`
+			LowResPath  string `json:"LowResPath"`
+		} `json:"Photo"`
+	} `json:"Property"`
+	PublicRemarks      string `json:"PublicRemarks"`
+	RelativeDetailsURL string `json:"RelativeDetailsURL"`
+}
+
+type mlsResponse struct {
+	Results []MLSListing `json:"Results"`
+}
+
+func newId() string {
+	var buf [16]byte
+	rand.Read(buf[:])
+	return hex.EncodeToString(buf[:])
+}
+
+func getMLS(id string) (*MLSListing, error) {
+	postData := url.Values{}
+	postData.Set("CultureId", "1")
+	postData.Set("ApplicationId", "1")
+	postData.Set("ReferenceNumber", id)
+	postData.Set("IncludeTombstones", "1")
+	postData.Set("Version", "6.0")
+	postData.Set("GUID", newId())
+	fmt.Println("FETCHING:", id)
+	resp, err := http.PostForm("https://api2.realtor.ca/Listing.svc/PropertySearch_Post", postData)
+	if err != nil {
+		fmt.Println("ERR:", err)
+		return nil, err
+	}
+	fmt.Println("Got ", id)
+	var listings mlsResponse
+	json.NewDecoder(resp.Body).Decode(&listings)
+	if len(listings.Results) > 0 {
+		return &listings.Results[0], nil
+	}
+	return nil, fmt.Errorf("No result found")
 }
